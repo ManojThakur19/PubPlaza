e16922f407361b749cc2e6eebcc5dacbcdba89df
ManojThakur <mnjthakur19@gmail.com>
2018-05-30 09:05:06 -0700

    Add project files.

---
+++ PubPlaza/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PubPlaza.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
